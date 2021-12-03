66dadf79a6e9d5803cddf71b7d30727fce2499fb
devendra-june-29 <devendra.june.29@gmail.com>
2021-12-03 22:45:35 +0530

    New Branch Created and this is first comment in this

---
+++ src/main/java/Pages/NewPageAdded.java
@@ -0,0 +1,14 @@
+package Pages;
+
+public class NewPageAdded {
+	
+	
+	public void newPageAdded() {
+		
+		System.out.println("This is New Page Added.");
+		
+		System.out.println("2nd line of code");
+		
+	}
+
+}
